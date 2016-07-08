da5916060a7f348773b4f40669c7fb7a8d742362
ch0ndawg <choni0281@gmail.com>
2016-07-08 11:19:45 -0700

    a little less skeletal. basic typedef

--- src/median_degree.hpp
+++ src/median_degree.hpp
@@ -4,8 +4,13 @@
 #include <unordered_map>
 #include <list>
 #include <priority_queue>
+#include <string>
 #include "json.hpp"
 
+class GraphNode;
 class MedianDegreeApp {
-
+public:
+  using Graph = std::unordered_map<std::string, GraphNode>;
+private:
+  Graph vGraph;
 };
