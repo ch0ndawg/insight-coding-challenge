281ed4260a1f5bf83690b635cf2ec89e11782f86
ch0ndawg <choni0281@gmail.com>
2016-07-08 21:38:12 -0700

    fixed a lot of the interface and logistics of making it templated

--- src/treap.hpp
+++ src/treap.hpp
@@ -1,13 +1,13 @@
 //
-//  implicit_treap.h
+//  Treap.h
 //  hr_array_query
 //
 //  Created by Chris on 1/16/16.
 //  Copyright © 2016 Chris. All rights reserved.
 //
 
-#ifndef implicit_treap_h
-#define implicit_treap_h
+#ifndef Treap_h
+#define Treap_h
 #include <iostream>
 #include <cassert>
 #include <cstdlib>
@@ -20,7 +20,7 @@
 // In particular, we cannot use tree objects to refer to subtrees
 // Actually, a node itself is the true recursive data structure; tree objects merely provide interface.
 template <class Key, class Value, class Priority>
-struct node {
+struct TreapNode {
     Key key;
     Value val;
     const Priority &priority;
@@ -30,67 +30,72 @@ struct node {
     // extra size field for maintaining order statistics
     int size;
 
-    node(const Key& k, const Value& v, const Priority &p, node* l=nullptr, node*r = nullptr)
+    TreapNode(const Key& k, const Value& v, const Priority &p, node* l=nullptr, node*r = nullptr)
     : Key(k), val(v), left(l), right(r), size(1), priority(p)
     {}
 };
 
 // allow size computation with null nodes without extra conditionals cluttering up the code
-inline int sz(const std::unique_ptr<node> &t)
+template <class Key, class Value, class Priority>
+inline int sz(const std::unique_ptr<TreapNode<Key,Value,Priority>> &t)
 {
     return t?t->size:0;
 }
 
-class implicit_treap {
+template<class Key, class Value, class Priority>
+class Treap {
+    // unwieldy
+    using node = TreapNode<Key, Value, Priority>;
+
     std::unique_ptr<node> root;
     //void postorder_cleanup(node* curr);
     void in_order(std::ostream &os, const std::unique_ptr<node>&) const;
     node* get_node(int index) const; // basic iterator function; we may extend this but for now, privatize
 public:
-    implicit_treap() : root(nullptr) {}
+    Treap() : root(nullptr) {}
 
     // move construct from node: this is will be used to prune a tree at some child node
     // implementation in terms of unique_ptr disallows tree objects to refer to a
     // proper subtree of another tree: a tree object always contains a root whose pointer
     // is not owned by anyone else, such as the child node of another tree.
     // (to enable such semantics, we would use shared_ptr's)
-    implicit_treap(std::unique_ptr<node>&&);
+    Treap(std::unique_ptr<node>&&);
 
-    implicit_treap(std::istream& is, int N);
+    Treap(std::istream& is, int N);
 
     // deep copy ops
-    implicit_treap(const implicit_treap&);
-    implicit_treap& operator=(const implicit_treap&);
+    Treap(const Treap<Key, Value, Priority>&);
+    Treap<Key, Value, Priority>& operator=(const Treap<Key, Value, Priority>&);
 
     // move constructor: steal the pointer
-    implicit_treap(implicit_treap&& other);
-    implicit_treap& operator=(implicit_treap&& other);
+    Treap(Treap<Key, Value, Priority>&& other);
+    Treap<Key, Value, Priority>& operator=(Treap<Key, Value, Priority>&& other);
 
 
-    implicit_treap &insert(int data, int index=-1);
-    implicit_treap &remove(int index);
-    implicit_treap &merge(implicit_treap &rhs);
-    implicit_treap split(int index, int add=0);
-    implicit_treap split(std::unique_ptr<node> &curr);
+    Treap<Key, Value, Priority> &insert(const Key& k, const Value & data);
+    Treap<Key, Value, Priority> &remove(const Key& k);
+    Treap<Key, Value, Priority> &merge(Treap &rhs);
+  //  Treap<Key, Value, Priority> split(int index, int add=0);
+    Treap<Key, Value, Priority> split(std::unique_ptr<node> &curr);
 
 
 
     void in_order(std::ostream &os) const { in_order(os,root); }
     int size() const { return sz(root); }
     const int &operator[](int index) const {return get_node(index)->val; } // array indexing
-    int &operator[](int index) { return const_cast<int&>(static_cast<const implicit_treap&>(*this)[index]); }
+    int &operator[](int index) { return const_cast<int&>(static_cast<const Treap<Key, Value, Priority>&>(*this)[index]); }
     const int &at(int index) const; // array indexing, checked
-    int &at(int index) { return const_cast<int&>(static_cast<const implicit_treap&>(*this).at(index)); }
+    int &at(int index) { return const_cast<int&>(static_cast<const Treap<Key, Value, Priority>&>(*this).at(index)); }
 
     int getkey(node* curr) const ;
-    ~implicit_treap();
+    ~Treap();
 
     bool empty() { return !root;}
 };
 
-inline std::ostream &operator <<(std::ostream&os, const implicit_treap& it)
+inline std::ostream &operator <<(std::ostream&os, const Treap& it)
 {
     it.in_order(os);
     return os;
 }
-#endif /* implicit_treap_h */
+#endif /* Treap_h */
