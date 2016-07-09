48325a63a30ab1724bd74b2948b4060853032642
ch0ndawg <choni0281@gmail.com>
2016-07-08 21:09:38 -0700

    redid the node interface.

--- src/treap.hpp
+++ src/treap.hpp
@@ -19,14 +19,19 @@
 // or one and only one parent node.
 // In particular, we cannot use tree objects to refer to subtrees
 // Actually, a node itself is the true recursive data structure; tree objects merely provide interface.
+template <class Key, class Value, class Priority>
 struct node {
-    int val;
-    std::unique_ptr<node> left;
-    std::unique_ptr<node> right;
+    Key key;
+    Value val;
+    const Priority &priority;
+    std::unique_ptr<node<Key,Value,Priority>> left;
+    std::unique_ptr<node<Key,Value,Priority>> right;
+
+    // extra size field for maintaining order statistics
     int size;
-    int priority;
-    
-    node(int v=0, node* l=nullptr, node*r = nullptr) : val(v), left(l), right(r), size(1), priority(std::rand())
+
+    node(const Key& k, const Value& v, const Priority &p, node* l=nullptr, node*r = nullptr)
+    : Key(k), val(v), left(l), right(r), size(1), priority(p)
     {}
 };
 
@@ -43,43 +48,43 @@ class implicit_treap {
     node* get_node(int index) const; // basic iterator function; we may extend this but for now, privatize
 public:
     implicit_treap() : root(nullptr) {}
-    
+
     // move construct from node: this is will be used to prune a tree at some child node
     // implementation in terms of unique_ptr disallows tree objects to refer to a
     // proper subtree of another tree: a tree object always contains a root whose pointer
     // is not owned by anyone else, such as the child node of another tree.
     // (to enable such semantics, we would use shared_ptr's)
     implicit_treap(std::unique_ptr<node>&&);
-    
+
     implicit_treap(std::istream& is, int N);
-    
+
     // deep copy ops
     implicit_treap(const implicit_treap&);
     implicit_treap& operator=(const implicit_treap&);
-    
+
     // move constructor: steal the pointer
     implicit_treap(implicit_treap&& other);
     implicit_treap& operator=(implicit_treap&& other);
-    
-    
+
+
     implicit_treap &insert(int data, int index=-1);
     implicit_treap &remove(int index);
     implicit_treap &merge(implicit_treap &rhs);
     implicit_treap split(int index, int add=0);
     implicit_treap split(std::unique_ptr<node> &curr);
-    
-    
-    
+
+
+
     void in_order(std::ostream &os) const { in_order(os,root); }
     int size() const { return sz(root); }
     const int &operator[](int index) const {return get_node(index)->val; } // array indexing
     int &operator[](int index) { return const_cast<int&>(static_cast<const implicit_treap&>(*this)[index]); }
     const int &at(int index) const; // array indexing, checked
     int &at(int index) { return const_cast<int&>(static_cast<const implicit_treap&>(*this).at(index)); }
-    
+
     int getkey(node* curr) const ;
     ~implicit_treap();
-    
+
     bool empty() { return !root;}
 };
 
