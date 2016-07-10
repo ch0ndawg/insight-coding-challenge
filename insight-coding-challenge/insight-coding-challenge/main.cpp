a9640ed6e95b2acac5c72ae69b57c78355d65f67
ch0ndawg <choni0281@gmail.com>
2016-07-10 13:28:00 -0700

    Beginning interface for the 3 structures version

--- insight-coding-challenge/insight-coding-challenge/main.cpp
+++ insight-coding-challenge/insight-coding-challenge/main.cpp
@@ -8,14 +8,9 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <string>
 #include "treap.hpp"
 
-class Random {
-    int val;
-public:
-    Random() : val(std::rand()) {}
-    bool operator<(Random rhs) { return val < rhs.val;}
-};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
@@ -27,6 +22,35 @@ int main(int argc, const char * argv[]) {
     }
     
     std::cout << t << std::endl;
-    std::cout << t.orderStatistic(0).second << ' ' << t.orderStatistic(10).second;
+    std::cout << t.orderStatistic(0).second << ' ' << t.orderStatistic(10).second << std::endl;
+    
+    Treap<std::string, int, Random> tree2;
+    
+    for (int i=0; i < 3; i++) {
+        std::string s;
+        int j;
+        std::cout << "Enter name: ";
+        getline(std::cin, s);
+        std::cout << "Enter number of points: ";
+        std::cin >> j;
+        std::cin.ignore(); // discard the newline
+        tree2.insert(s,j);
+    }
+    
+    std::string s2;
+    std::cout << "What would you like to delete? ";
+    getline(std::cin,s2);
+    
+    tree2.remove(s2);
+    
+    for (int i=0; i< tree2.size(); i++) {
+        auto curr = tree2.orderStatistic(i);
+        std::cout << curr.first << ": " << curr.second << std::endl;
+    }
+    std::pair<int, std::string> k1 = std::make_pair(3, "A");
+    std::pair<int, std::string> k2 = std::make_pair(3, "B");
+    
+    if (k1 < k2) std::cout << "A is less";
+    else std::cout << "B is less";
     return 0;
 }
--- src/median_degree.hpp
+++ src/median_degree.hpp
@@ -5,13 +5,34 @@
 #include <priority_queue>
 #include <string>
 #include <chrono>
+#include <utility>
 #include "json.hpp"
+#include "treap.hpp"
 
-class GraphVertex; // Graph vertex: contains a priority queue of transactions
+class Random {
+    int val;
+public:
+    Random() : val(std::rand()) {}
+    bool operator<(Random rhs) { return val < rhs.val;}
+};
+
+// this will be the key for the median updates tree. It is ordered lexicographically.
+using DegName = std::pair<int, std::string>;
+class JsonDateCompare {
+    bool operator(const nhlohmann::json &lhs, const nhlohmann::json& rhs) {
+        return lhs["Date"] < rhs["Date"];
+    }
+};
 
 class MedianDegreeApp {
 public:
-  using Graph = std::unordered_map<std::string, nlohmann::json>;
+    // Will branch to make a version that uses dates as priorities, eliminating this
+    using TransactionList = std::priority_queue<nlohmann::json, std::vector<nlohmann::json>, JsonDateCompare>;
+    using DegreeMap = std::unordered_map<std::string,int>;
+    // actually, the values are not used. Some tweaking, perhaps, for a "Set Treap"
+    // Treap by the strict definition: using a randomly generated priority
+    // Will branch to make a version that uses dates as priorities, eliminating TransactionList
+    using MedianMap = std::Treap<DegName,int,Random>
 private:
 
   Graph vGraph;
