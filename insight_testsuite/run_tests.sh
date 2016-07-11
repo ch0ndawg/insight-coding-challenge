b00257dbec61726cb4c7d288c65210e12c7f3579
ch0ndawg <choni0281@gmail.com>
2016-07-10 22:58:55 -0700

    run test modified for binary

--- insight_testsuite/run_tests.sh
+++ insight_testsuite/run_tests.sh
@@ -44,7 +44,7 @@ function setup_testing_input_output {
 
   mkdir -p ${TEST_OUTPUT_PATH}
 
-  cp -r ${PROJECT_PATH}/src ${TEST_OUTPUT_PATH}
+  cp -r ${PROJECT_PATH}/bin ${TEST_OUTPUT_PATH}
   cp -r ${PROJECT_PATH}/run.sh ${TEST_OUTPUT_PATH}
   cp -r ${PROJECT_PATH}/venmo_input ${TEST_OUTPUT_PATH}
   cp -r ${PROJECT_PATH}/venmo_output ${TEST_OUTPUT_PATH}
